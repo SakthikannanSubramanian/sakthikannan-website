a762e10ea7c4965722785b3adbb0a2031c53e2ca
Sakthikannan Subramanian <40839233+SakthikannanSubramanian@users.noreply.github.com>
2024-06-14 20:30:30 +0100

    Import changes to avoid vulnerability

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faLinkedin, faTwitter, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faTwitter, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 const contactData = {
   "email": "sakthikannan.subramanian@gmail.com",
